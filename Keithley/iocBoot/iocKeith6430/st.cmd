3fb56da7022dd5b9feb324cab803a167d71084eb
gkufatty <gkufatty@fsu.edu>
2026-08-25 16:39:11 +0200

    update Keithley ioc

--- Keithley/iocBoot/iocKeith6430/st.cmd
+++ Keithley/iocBoot/iocKeith6430/st.cmd
@@ -1,30 +1,30 @@
-#!../../bin/darwin-aarch64/streamloc
+#!../../bin/linux-x86_64/keith6430
 
 #- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
 #-
 #- SPDX-License-Identifier: EPICS
 
-#- You may have to change streamloc to something else
+#- You may have to change keith6430 to something else
 #- everywhere it appears in this file
 
 < envPaths
 
 ## Register all support components
-dbLoadDatabase "../../dbd/streamloc.dbd"
-streamloc_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase "../../dbd/keith6430.dbd"
+keith6430_registerRecordDeviceDriver(pdbbase) 
 
 epicsEnvSet "STREAM_PROTOCOL_PATH", "$(TOP)/db"
 epicsEnvSet PORT port
 
 # drvAsynIPPortConfigure "$(PORT)", "localhost:5555"
-# dbLoadRecords("$(TOP)/db/stream.db", "P=gk:, PORT=port")
+# dbLoadRecords("$(TOP)/db/stream.db", "P=ess:, PORT=port")
 drvAsynIPPortConfigure "port", "ics-lab-moxa.cslab.esss.lu.se:4001"
 
-dbLoadRecords("$(TOP)/db/stream.db", "P=gk:, PORT=port")
+dbLoadRecords("$(TOP)/db/stream.db", "P=LaserDiffraction, R=Keithley, PORT=port")
 ## Load record instances
-#dbLoadRecords("../../db/streamloc.db","user=gk")
+#dbLoadRecords("../../db/keith6430.db","user=ess")
 
 iocInit()
 
 ## Start any sequence programs
-#seq sncstreamloc,"user=gk"
+#seq snckeith6430,"user=ess"
