24aa23901777bd8358bc6b91c0c5ed9ee826f294
David-Andrino <david.andrino@upm.es>
2026-08-25 11:47:59 +0200

    Create camera-control IOC

---
+++ camera-control/iocBoot/ioccamera-control/st.cmd
@@ -0,0 +1,25 @@
+#!../../bin/linux-x86_64/camera-control
+
+#- SPDX-FileCopyrightText: 2003 Argonne National Laboratory
+#-
+#- SPDX-License-Identifier: EPICS
+
+#- You may have to change camera-control to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/camera-control.dbd"
+camera_control_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/camera-control.db","user=epicsstudent")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=epicsstudent"
