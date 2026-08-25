60feaf86de89a4f599328e3d429b63a6579ece16
David-Andrino <david.andrino@upm.es>
2026-08-25 11:37:11 +0200

    Add green and yellow LEDs

--- laser/iocBoot/iocpi-laser/st.cmd
+++ laser/iocBoot/iocpi-laser/st.cmd
@@ -16,12 +16,20 @@ dbLoadDatabase "dbd/pi-laser.dbd"
 pi_laser_registerRecordDeviceDriver pdbbase
 
 epicsEnvSet("LASER_PORT", "asynLaser")
-asynGPiOConfigure("$(LASER_PORT)", 1, 1) # PWM Pin 1 (GPIO 19)
+asynGPiOConfigure("$(LASER_PORT)", 1, 1) # WPi Pin 1 (GPIO 19)
 dbLoadRecords("db/GPiO.db","P=LaserDiffraction, R=Laser, PORT=$(LASER_PORT)")
 
-epicsEnvSet("LED_PORT", "asynLED")
-asynGPiOConfigure("$(LED_PORT)", 25, 0) # GPIO Pin 25 (GPIO 26)
-dbLoadRecords("db/BlinkingLEDs.db","P=LaserDiffraction, R=LED, PORT=$(LED_PORT)")
+epicsEnvSet("RED_PORT", "asynRed")
+asynGPiOConfigure("$(RED_PORT)", 25, 0) # WPi Pin 25 (GPIO 26)
+dbLoadRecords("db/BlinkingLEDs.db","P=LaserDiffraction, R=RedLED, PORT=$(RED_PORT)")
+
+epicsEnvSet("GREEN_PORT", "asynGreen")
+asynGPiOConfigure("$(GREEN_PORT)", 7, 0) # WPi Pin 7 (GPIO 4)
+dbLoadRecords("db/BlinkingLEDs.db","P=LaserDiffraction, R=GreenLED, PORT=$(GREEN_PORT)")
+
+epicsEnvSet("YELLOW_PORT", "asynYellow")
+asynGPiOConfigure("$(YELLOW_PORT)", 24, 1) # WPi Pin 24 (GPIO 19)
+dbLoadRecords("db/GPiO.db","P=LaserDiffraction, R=YellowLED, PORT=$(YELLOW_PORT)")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
