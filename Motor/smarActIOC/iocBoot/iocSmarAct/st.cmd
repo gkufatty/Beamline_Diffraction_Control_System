7d1508645236391172cea4a42dba6a72a77e52d0
georkufatty <gkuaftty@fsu.edu>
2026-08-26 14:23:54 +0200

    motor ioc

--- Motor/smarActIOC/iocBoot/iocSmarAct/st.cmd
+++ Motor/smarActIOC/iocBoot/iocSmarAct/st.cmd
@@ -1,7 +1,7 @@
 #!../../bin/linux-x86_64/smarAct
 
 < envPaths
-epicsEnvSet("P", "smarAct:")
+epicsEnvSet("P", "SMARACT:")
 epicsEnvSet("MC_CT", "unit1")
 
 cd "${TOP}"
@@ -13,7 +13,7 @@ smarAct_registerRecordDeviceDriver pdbbase
 cd "${TOP}/iocBoot/${IOC}"
 
 ## motorUtil (allstop & alldone)
-dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=smarAct:")
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=SMARACT:")
 
 # Uncomment one of the following lines for MCS or MCS2 controller
 < smaractmcs.iocsh
@@ -23,18 +23,9 @@ dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=smarAct:")
 # Optional: load devIocStats records (requires DEVIOCSTATS module)
 #dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(P)$(MC_CT)")
 
-# autosave/restore machinery
-#<AutoSave.cmd
-
-
 iocInit
 
 ## motorUtil (allstop & alldone)
-motorUtilInit("smarAct:")
-
-# save motor positions every five seconds
-#create_monitor_set("auto_positions.req",5,"P=$(P)")
-# save settings every thirty seconds
-#create_monitor_set("auto_settings.req",30,"P=$(P)")
+motorUtilInit("SMARACT:")
 
 # Boot complete
