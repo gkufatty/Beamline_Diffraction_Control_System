8cbf39b3c69f84a72a5b31b2de32e865a4c4c905
David-Andrino <david.andrino@upm.es>
2026-08-26 10:58:22 +0200

    Add autosave support to the camera control

--- camera-control/iocBoot/ioccamera-control/st.cmd
+++ camera-control/iocBoot/ioccamera-control/st.cmd
@@ -20,11 +20,18 @@ epicsEnvSet "PORT" "portname"
 drvAsynIPPortConfigure "$(PORT)", "epicsss-axiscamera-01.cslab.esss.lu.se:80 HTTP"
 dbLoadRecords("db/cameracontrol.db","P=LaserDiffraction,R=CameraControl,PORT=$(PORT)")
 
+set_requestfile_path("$(TOP)/db")
+set_savefile_path("$(TOP)/db")
+set_pass0_restoreFile("cameracontrol.sav", "P=LaserDiffraction,R=CameraControl")
+set_pass1_restoreFile("cameracontrol.sav", "P=LaserDiffraction,R=CameraControl")
+save_restoreSet_NumSeqFiles(0)
+
 ## Load record instances
 #dbLoadRecords("db/camera-control.db","user=epicsstudent")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
+create_monitor_set("cameracontrol.req", 5, "P=LaserDiffraction,R=CameraControl")
 ## Start any sequence programs
 #seq sncxxx,"user=epicsstudent"
